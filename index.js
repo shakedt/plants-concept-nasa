a9fc20237ad2c5243c9a397a9890c8694ece419c
shaked tayeb <dokuroc@gmail.com>
2021-04-30 11:16:45 +0300

    add basic logic of habitable plants

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const fs = require('fs');
 
 const results = [];
 
+const isHabitablePlanet = (planet) => {
+    const isConfirmed  = planet['koi_disposition'] === 'CONFIRMED';
+    const isLow = planet['koi_insol'] > 0.36;
+    const isHigh = planet['koi_insol'] < 1.11;
+    const radius = planet['koi_prad'] < 1.6;
+
+    return isConfirmed && isLow && isHigh && radius; 
+}
+
 const stream = fs.createReadStream('./kepler_data.csv')
     .pipe(csvParse({
         comment: '#',
@@ -10,16 +19,16 @@ const stream = fs.createReadStream('./kepler_data.csv')
     }));
 
 stream.on('data', (data) => {
-  results.push(data);
+    if (isHabitablePlanet(data)) results.push(data);
 });
 
 stream.on('end', () => {
-  console.log('so done wow');
   console.log(results);
+  console.log(`${results.length} is the amount of plants that where found`);
 });
 
 stream.on('error', (err) => {
   console.log(err);
 });
 
-// const data = parse();
+
